69948772ca2c01edca5304b672bfcd97cc31f57f
Alexandra Klimenko <klimenko@usc.edu>
2017-05-20 04:21:35 -0700

    Installs

--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -1,10 +1,20 @@
 export class MainController {
-  constructor ($timeout, webDevTec, toastr) {
+  constructor ($http) {
     'ngInject';
 
-  }
-  postMessage() {
-    console.log("post");
-  }
-}
+      this.$http = $http;
+    //   this.getMessages();
 
+  }
+    // getMessages() {
+    //   var vm = this;
+    //     this.$http.post('http://localhost:5000/api/message').then(function(result){
+    //         vm.message = result.data;
+    //     });
+    // }
+
+    postMessage() {
+        this.$http.post('http://localhost:5000/api/message', {msg: this.message});
+    }
+
+}
